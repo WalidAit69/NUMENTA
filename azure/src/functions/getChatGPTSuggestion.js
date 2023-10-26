f15262c3cac5a300600648a651c6d7c589434672
WalidAit69 <aitwalid2000@gmail.com>
2023-10-26 06:17:15 -0700

    suggestions

--- azure/src/functions/getChatGPTSuggestion.js
+++ azure/src/functions/getChatGPTSuggestion.js
@@ -1,13 +1,28 @@
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
+const openai = require("../../lib/openai");
 
-app.http('getChatGPTSuggestion', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
+app.http("getChatGPTSuggestion", {
+    methods: ["GET"],
+    authLevel: "anonymous",
     handler: async (request, context) => {
+        const response = await openai.chat.completions.create({
+            messages: [
+                { role: "user", content: "Write a random text prompt for DALL-E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract,modern, black and white etc. Do not wrap the answer in quotes." }
+            ], 
+            model: "gpt-3.5-turbo",
+            max_tokens: 50,
+            temperature:0.8,
+        });
+
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        const suggestion = response?.choices[0]?.message.text;
 
-        return { body: `Hello, ${name}!` };
-    }
+        return {
+            body: suggestion,
+        };
+    },
 });
+
+
+
--- src/components/PromptInput.jsx
+++ src/components/PromptInput.jsx
@@ -8,9 +8,11 @@ function PromptInput() {
 
     const [input, setinput] = useState('');
 
-    const {data:suggestion , isLoading , mutate , isValidating} = useSWR('/api/suggestion' , fetchSuggestion ,{
-        revalidateOnFocus:false
+    const { data: suggestion, isLoading, mutate, isValidating } = useSWR('/api/suggestion', fetchSuggestion, {
+        revalidateOnFocus: false
     })
+
+    const loading = isLoading || isValidating;
 
     return (
         <section className="m-10">
@@ -18,14 +20,16 @@ function PromptInput() {
                 <textarea
                     value={input}
                     onChange={(e) => setinput(e.target.value)}
-                    placeholder="Enter a prompt..."
-                    className="flex-1 p-4 rounded-md outline-blue-400" name="" id=""></textarea>
+                    placeholder={
+                        (loading && "Numenta is thinking...") ||
+                        suggestion || "Enter a prompt..."}
+                    className="flex-1 p-4 rounded-md outline-blue-400 text-sm" name="" id=""></textarea>
 
-                <button 
-                disabled={!input}
-                type="submit" 
-                className={`p-4 font-semibold ${input ? 'bg-blue-800 text-white transition-colors duration-200' 
-                : 'text-gray-300 cursor-not-allowed'}`}
+                <button
+                    disabled={!input}
+                    type="submit"
+                    className={`p-4 font-semibold ${input ? 'bg-blue-800 text-white transition-colors duration-200'
+                        : 'text-gray-300 cursor-not-allowed'}`}
                 >Generate</button>
 
                 <button
@@ -33,10 +37,18 @@ function PromptInput() {
                     className="p-4 bg-blue-400 text-white transition-colors duration-200 font-semibold
             disabled:cursor-not-allowed disabled:bg-gray-400">Use Suggestion</button>
                 <button
+                    onClick={mutate}
                     type="button"
                     className="p-4 bg-white text-blue-400 transition-colors duration-200 font-semibold
             rounded-b-md md:rounded-r-md md:rounded-bl-none">New Suggestion</button>
             </form>
+
+            {input && (
+                <p className="mt-2 font-light">
+                    Suggestion:
+                    <span className="text-blue-400">{loading ? "Numenta is thinking..." : suggestion}</span>
+                </p>
+            )}
         </section>
     )
 }
